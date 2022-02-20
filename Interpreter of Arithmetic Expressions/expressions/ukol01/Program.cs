bf93ca072dd0624acfa23af36d3c054a03433a71
Filip Sedlařík <filip.sedlarik@gmail.com>
2022-02-20 20:40:26 +0100

    first commit

---
+++ Interpreter of Arithmetic Expressions/expressions/ukol01/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace ukol01
+{
+    class Program
+    {
+        static char[] expressions = { '+', '-', '*', '/' };
+
+        static string removeSpaces(string line) {
+            if (line.Contains(" "))
+            {
+                line = line.Replace(" ", "");
+                return line;
+            }
+            return line;
+        }
+
+        static string getResultOperator(string line) {
+            int index = 0;
+            string firstNum = null;
+            string secondNum = null;
+            string anotherNum = null;
+            string operat = null;
+            int startIndex = 0;
+
+            int lparCount = line.Count(par => par == '(');
+            int pparCount = line.Count(par => par == ')');
+
+
+            while (true) {
+                
+                while (index != line.Length) 
+                {
+                    //first number
+                    if (((line[index] >= '0' && line[index] <= '9')) && (firstNum == null))
+                    {
+                        while (line[index] >= '0' && line[index] <= '9')
+                        {
+                            firstNum += line[index++];
+                            startIndex++;
+                            if (index >= line.Length) { break; }
+                        }
+                    }
+                    //second number
+                    else if (((line[index] >= '0' && line[index] <= '9')) && (secondNum == null))
+                    {
+                        while (line[index] >= '0' && line[index] <= '9')
+                        {
+                            secondNum += line[index++];
+                            startIndex++;
+                            if (index >= line.Length) { break; }
+                        }
+                    }
+                    //another number
+                    else if (((line[index] >= '0' && line[index] <= '9')) && (anotherNum == null))
+                    {
+                        while (line[index] >= '0' && line[index] <= '9')
+                        {
+                            anotherNum += line[index++];
+                            startIndex++;
+                            if (index >= line.Length) { break; }
+                        }
+                    }
+                    //first operator
+                    else if (((line[index] == '*' || line[index] == '/' || line[index] == '+' || line[index] == '-')) && (operat == null))
+                    {
+                        operat = line[index++].ToString();
+                        startIndex++;
+                        if (index >= line.Length) { Console.WriteLine("error"); return "err"; }
+                    }
+                    //other operators
+                    else if (((operat == "+") || (operat == "-")) && ((line[index] == '/') || (line[index] == '/')))
+                    {
+                        if (anotherNum != null)
+                        {
+                            firstNum = anotherNum;
+                            anotherNum = null;
+                        }
+                        else { firstNum = secondNum; }
+
+                        secondNum = null;
+                        operat = line[index].ToString();
+                        startIndex++;
+                        index++;
+
+                        if (index >= line.Length)
+                        {
+                            Console.WriteLine("err");
+
+                        }
+                    }
+                    else if (line[index] == '(')
+                    {
+                        firstNum = null;
+                        secondNum = null;
+                        operat = null;
+
+                        index++;
+                    }
+                    else if (line[index] == ')')
+                    {
+                        index++;
+                        break;
+                    }
+                    else
+                    {
+                        index++;
+                    }
+                }
+
+                if (secondNum == null)
+                {
+                    return firstNum;
+                }
+
+                if (lparCount != pparCount) {
+                    Console.WriteLine("different count of brackets!");
+                    return "error";
+                }
+
+                
+
+                string result = doMathStr(int.Parse(firstNum), int.Parse(secondNum), char.Parse(operat));
+                //nalezena chyba pri prikladu 6 * 2 + (5 - 3) * 3 - 8 to sice prvni udela zavorku, ale potom to normalne pocita zleva doprava, tudiz misto vysledku 10 je to 34
+
+                line = line.Replace(firstNum+operat+secondNum,result);
+                if (int.Parse(result) < 0) {
+                    Console.WriteLine("bad expression");
+                    return "ERROR";
+                }
+                if ((lparCount > 0) && line.Contains("(" + int.Parse(result) + ")"))
+                {
+                    line = line.Replace("(" + result + ")", result);
+                    lparCount--;
+                    pparCount--;
+                }
+
+                firstNum = null;
+                secondNum = null;
+                anotherNum = null;
+                operat = null;
+                index = 0;
+            }
+        }
+
+        static string doMathStr(int a, int b, char operation)
+        {
+            
+            switch (operation)
+            {
+                case '*':
+                    return (a * b).ToString();
+                case '+':
+                    return (a + b).ToString();
+
+                case '-':
+                    return (a - b).ToString();
+      
+                case '/':
+                    return (a / b).ToString();
+                default:
+                    return "si kojot";
+ 
+            }
+        }
+        static int calculate(string line)
+        {
+            List<string> znamenka = new List<string>();
+            int startIndex, endIndex;
+            int a, b;
+            int result = 0;
+            char operation;
+            string nwm = null;
+
+  
+            nwm = getResultOperator(line);
+            if (nwm == "ERROR")
+            {
+                Console.WriteLine("Error in computing expression");
+                Environment.Exit(0);
+            }
+            else { 
+                return int.Parse(nwm);
+            }
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            bool contin = true;
+            while (contin) {
+                
+                bool userInput = false;
+                int countings = 0;
+                while (userInput == false) {
+                    try
+                    {
+                        Console.WriteLine("How many countings you want? Type number");
+                        countings = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    if (countings >= 1)
+                    {
+                        userInput = true;
+                    }
+                }
+
+
+                for (int i = 0; i < countings; i++)
+                {
+                    string line;
+                    Console.WriteLine("Enter matemathical operation");
+                    line = Console.ReadLine();
+                    if (string.IsNullOrEmpty(line)) {
+                        Console.WriteLine("Empty input!");
+                        Environment.Exit(0);
+                    }
+                    string lineWithoutSpaces = removeSpaces(line);
+                    if (lineWithoutSpaces[0] == '-' || lineWithoutSpaces[0] == '+' || lineWithoutSpaces[0] == '/' || lineWithoutSpaces[0] == '*') { Console.WriteLine("You cannot use negative integer!"); Environment.Exit(0); }
+
+                    int tmp = calculate(lineWithoutSpaces);
+                    Console.WriteLine($"result: {tmp}");
+                    
+                }
+
+                Console.WriteLine("Do you want to continue? If yes, type yes, otherwise type anything to exit\n");
+                string answer = Console.ReadLine();
+                if (answer.ToLower().Contains("yes") || answer.ToUpper().Contains("YES"))
+                {
+                    contin = true;
+                }
+                else
+                {
+                    contin = false;
+                }
+
+
+            }
+        }
+    } 
+}
---
+++ Lexical Analyzer/interpreter/lab02/Program.cs
@@ -0,0 +1,154 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace lab02 {
+    
+    internal class Program
+    {
+        static char[] operators = {'+', '-', '*', '/' };
+        string[] delimiters = { "(", ")", ";" };
+        static string[] keywords = {  "mod" , "div"};
+
+        static string removeWhiteSpaces(string line) {
+            return line.Replace(" ", "");
+        }
+
+        static void doOperations(string line) {
+            string numberStr = string.Empty;
+            string others = string.Empty;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+
+                if (line[i] == '(')
+                {
+                    Console.WriteLine("LPAR");
+                    continue;
+                }
+                if (line[i] == ')')
+                {
+                    Console.WriteLine("RPAR");
+                    continue;
+                }
+                if (line[i] == ';')
+                {
+                    Console.WriteLine("SEMICOLON");
+                }
+                if (line[i] >= '0' && line[i] <= '9')
+                {
+                    if (!(line[i + 1] >= '0' && line[i + 1] <= '9'))
+                    {
+                        numberStr += line[i];
+                        Console.WriteLine($"NUM: {numberStr}");
+                        numberStr = string.Empty;
+                    }
+                    else
+                    {
+                        numberStr += line[i];
+                    }
+                }
+                if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z'))
+                {
+                    if (i + 1 < line.Length)
+                    {
+                        if (((line[i + 1] >= 'a' && line[i + 1] <= 'z') || (line[i + 1] >= 'A' && line[i + 1] <= 'Z')))
+                        {
+                            others += line[i];
+                        }
+                        else
+                        {
+                            others += line[i];
+                            if (others.Contains("div") || others.Contains("mod"))
+                            {
+                                others = string.Empty;
+
+                            }
+                            else
+                            {
+                                Console.WriteLine($"ID: {others}");
+                                others = string.Empty;
+                            }
+                        }
+                    }
+                    else {
+                        others += line[i];
+                        Console.WriteLine($"ID: {others}");
+                    }
+                }
+                foreach (var item in operators)
+                {
+                    if (line[i] == item)
+                    {
+                        Console.WriteLine($"OP: {item}");
+                    }
+                }
+
+                foreach (var item in keywords)
+                {
+                    if (i + item.Length > line.Length)
+                    {
+                        break;
+                    }
+                    var subStr = line.Substring(i, item.Length);
+                    if (subStr == item)
+                    {
+                        Console.WriteLine(subStr);
+                    }
+                }
+            }
+        }
+
+        static void handleTokens(string line) {
+
+            string lineWithoutSpaces = removeWhiteSpaces(line);
+            string newStr = string.Empty;
+            string result = string.Empty;
+            if (lineWithoutSpaces.Contains("//"))
+            {
+                result = line.Split("//").Last();
+                newStr = line.Split("//").First();
+                result = result.Replace(" ", "");
+            }
+
+            if (!string.IsNullOrEmpty(newStr))
+            {
+                doOperations(newStr);
+            }
+            else {
+                doOperations(lineWithoutSpaces);
+            }
+
+            if (!string.IsNullOrEmpty(result)) {
+                Console.WriteLine($"NOTE: {result.ToUpper()}");
+            }
+            
+        }
+        static void Main(string[] args) {
+            Console.WriteLine("How many lexical senteces you want to input?");
+            int countings = 0;
+            try
+            {
+                 countings= int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+           
+            for (int i = 0; i < countings; i++)
+            {
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("empty input, exiting program");
+                    Environment.Exit(1);
+                }
+                else {
+                    handleTokens(input);
+                }
+            }
+        }
+    }
+}
